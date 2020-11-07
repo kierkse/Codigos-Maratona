477c9933fb0d6aca5c92eac4b2a8feb8e4f7e44b
MeriLOW <74102254+MeriLOW@users.noreply.github.com>
2020-11-07 14:34:54 -0300

    Add files via upload

---
+++ Problema_F_EscolaDeMusica.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+
+   float soma = 0.0, mediana = 0.0, media = 0.0, aux = 0.0;
+   int alunos[600], moda [600], i = 0, j = 0, N = 0, mod1 = 0, mod2 = 0, preenc = 0, mo = 0, tammoda = 0;
+   do {
+        printf("Digite a quantidade de alunos: ");
+        scanf("%d", &N);
+    } while(0 >= N > 600);
+	do {
+        printf("\nDigite a idade do %do aluno: ", i+1);
+        scanf("%d", &alunos[i]);
+        if (2 <= alunos[i] <= 18) {
+        	i++;
+        	preenc++;
+		}
+	} while (preenc < N);
+	i = 0;
+	for (i = 0; i < N; i++) {
+		soma += alunos[i];
+	}
+	media = soma / N;
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            if(alunos[i] < alunos[j]){
+                aux = alunos[i];
+                alunos[i] = alunos[j];
+                alunos[j] = aux;
+            }
+        }
+    }
+    for(i = 0; i < N - 1; i++){
+		for(j = i + 1; j < N; j++){
+			if(alunos[i] == alunos[j])
+				mod1++;
+			if(mod1 > mod2) {
+				moda [mo] = alunos[i];
+				mo++;
+				tammoda++;
+			}
+			mod2 = mod1;
+			mod1 = 0;
+		}
+	}
+    if(N % 2)
+        mediana = alunos[N/2];
+    else
+        mediana = (alunos[N/2-1] + alunos[N/2])/2;
+    printf("\nMODA=%d", moda[0]);
+    for (i = 1; i < tammoda; i++) {
+		printf(", %d", moda[i]);
+	}
+    printf("\nMEDIA=%.2f", media);
+    printf("\nMEDIANA=%.2f", mediana);
+    return 0;
+}
