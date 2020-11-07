38a9e2388bdcf052e5cb1c40a741274d08d1fe41
Le Alves <60941523+kierkse@users.noreply.github.com>
2020-11-07 13:49:03 -0300

    Update acessibilidade.c

--- acessibilidade.c
+++ acessibilidade.c
@@ -9,14 +9,16 @@ int main() {
 	scanf("%lf",&L);
 	
 	while (!(H==0.0 && C==0.0 && L==0.0)) {
-		scanf("%lf",&H);
-		scanf("%lf",&C);
-		scanf("%lf",&L);
+		
 		i=H*100.0/C;
 		if (i<=8.334 && L>=0.8)
 			printf("PROJETO SIMPLES\n");
 		else
 			printf("PROJETO ESPECIAL\n");
+		
+		scanf("%lf",&H);
+		scanf("%lf",&C);
+		scanf("%lf",&L);
 	}
 	
 	return 0;
