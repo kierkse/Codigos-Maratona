d67f9b647544b2e19fc5a5ff7c3850938ab8ed67
gdosualdo <74100952+gdosualdo@users.noreply.github.com>
2020-11-07 14:00:23 -0300

    Update acessibilidade.c

--- acessibilidade.c
+++ acessibilidade.c
@@ -4,22 +4,18 @@ int main() {
 	double H, C, L;
 	double i;
 	
-	scanf("%lf",&H);
-	scanf("%lf",&C);
-	scanf("%lf",&L);
-	
-	while (!(H==0.0 && C==0.0 && L==0.0)) {
-		
-		i=H*100.0/C;
-		if (i<=8.334 && L>=0.8)
-			printf("PROJETO SIMPLES\n");
-		else
-			printf("PROJETO ESPECIAL\n");
-		
+	do{
 		scanf("%lf",&H);
 		scanf("%lf",&C);
 		scanf("%lf",&L);
-	}
+		i=H*100.0/C;
+		if (!(H==0.0 && C==0.0 && L==0.0))
+			if (i<=8.334 && L>=0.8)
+				printf("PROJETO SIMPLES");
+			else
+				printf("PROJETO ESPECIAL");
+		printf("\n");
+	} while (!(H==0.0 && C==0.0 && L==0.0));
 	
 	return 0;
 }
